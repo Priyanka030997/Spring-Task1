2f81c7569cdb74494dca55a131bf381f36416fdb
priyanka <priyanka150skct@gmail.com>
2019-07-19 09:43:03 +0530

    updated gitignore file and removed unwanted code

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -1,15 +1,7 @@
 package com.stackroute.domain;
 
-
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.beans.factory.BeanFactory;
-
 public class Main {
     public static void main(String args[])
     {
@@ -30,39 +22,6 @@ public class Main {
         movieF.display();
         Movie movie4=context.getBean("movieE",Movie.class);
         System.out.println(movieE==movie4);
-
-
-        /*//use Beanfactory
-       BeanFactory factory=new ClassPathXmlApplicationContext("beans.xml");
-        movie1=factory.getBean("movie1",Movie.class);
-        movie1.display();
-        movie2=factory.getBean("movie2",Movie.class);
-        movie2.display();
-        movie3=factory.getBean("movie3",Movie.class);
-        movie3.display();
-
-        //use BeanDefinitionReader
-        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        BeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
-        reader.loadBeanDefinitions(new FileSystemResource("src/main/resources/beans.xml"));
-        movie1= beanFactory.getBean("movie1",Movie.class);
-        movie1.display();
-        movie2= beanFactory.getBean("movie2",Movie.class);
-        movie2.display();
-        movie3= beanFactory.getBean("movie3",Movie.class);
-        movie3.display();
-
-        //use BeanDefinitionRegistry
-        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
-        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions(new FileSystemResource("src/main/resources/beans.xml"));
-        Movie moviebean1= beanFactory.getBean("movie1",Movie.class);
-        moviebean1.display();
-        Movie moviebean2= beanFactory.getBean("movie2",Movie.class);
-        moviebean2.display();
-        Movie moviebean3= beanFactory.getBean("movie3",Movie.class);
-        moviebean3.display();*/
-
 
     }
    }
