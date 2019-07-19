3aef049e5c2d4b28f2711956f59ccdf76d428310
priyanka <priyanka150skct@gmail.com>
2019-07-19 12:28:47 +0530

    updated gitignore file and added comments in all class

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -10,9 +10,6 @@ public class Actor
     private String gender;
     private int age;
 
-
-
-
     public Actor(String name, String gender, int age) {
         this.name = name;
         this.gender = gender;
@@ -21,7 +18,7 @@ public class Actor
    public Actor(){
 
     }
-
+//used setter and getter methods
     public void setName(String name) {
         this.name = name;
     }
